84a6c439b237f2d31f1165b79be6d9552e5bc817
Christopher Baker <me@christopherbaker.net>
2019-04-06 16:09:01 -0600

    Update nuts and bolts.

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxOscSerial
+	ADDON_DESCRIPTION = A serial osc addon for openFrameworks.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "serial" "osc"
+	ADDON_URL = http://github.com/bakercp/ofxOscSerial
+
+common:
+	ADDON_DEPENDENCIES = ofxSerial ofxIO ofxOsc
